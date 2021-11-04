7e113d7613428df4b9a0b43682615d7236e1d09a
Antonio Cuadros <acuadros1995@gmail.com>
2021-11-04 11:22:17 +0100

    Install ip route packet

--- test/test/client_agent/ClientAgentCan.hpp
+++ test/test/client_agent/ClientAgentCan.hpp
@@ -38,7 +38,7 @@ public:
     {
         if (!initialized)
         {
-            ASSERT_TRUE(0 == system("ip link add dev vcan0 type vcan && ip link set vcan0 mtu 72 && ip link set dev vcan0 up"));
+            ASSERT_TRUE(0 == system("apt install -y iproute2 && ip link add dev vcan0 type vcan && ip link set vcan0 mtu 72 && ip link set dev vcan0 up"));
             initialized = true;
         }
 
