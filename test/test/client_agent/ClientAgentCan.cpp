ed21fc14ac2d321b322bdd3dd0adc28313daaad3
Pablo Garrido <pablogs9@gmail.com>
2022-12-09 11:21:02 +0100

    Fix tests
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>

--- test/test/client_agent/ClientAgentCan.cpp
+++ test/test/client_agent/ClientAgentCan.cpp
@@ -9,7 +9,13 @@ TEST_P(ClientAgentCan, PingFromClientToAgent)
     ASSERT_NO_FATAL_FAILURE(client_can_.ping_agent());
 }
 
-INSTANTIATE_TEST_CASE_P(
+#ifdef INSTANTIATE_TEST_SUITE_P
+#define GTEST_INSTANTIATE_TEST_MACRO(x, y, z) INSTANTIATE_TEST_SUITE_P(x, y, z)
+#else
+#define GTEST_INSTANTIATE_TEST_MACRO(x, y, z) INSTANTIATE_TEST_CASE_P(x, y, z)
+#endif // ifdef INSTANTIATE_TEST_SUITE_P
+
+GTEST_INSTANTIATE_TEST_MACRO(
     CanTransports,
     ClientAgentCan,
     ::testing::Values(MiddlewareKind::FASTDDS));
